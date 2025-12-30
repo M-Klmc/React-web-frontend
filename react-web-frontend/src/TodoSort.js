9b81e854b4de9bbbf33cf6f7a9c8772792dea348
Mikhail <041411032425mk@gmail.com>
2025-12-30 16:29:09 +0400

    added TodoSearch and TodoSort

---
+++ react-web-frontend/src/TodoSort.js
@@ -0,0 +1,16 @@
+export default function TodoSort ({ sort, acceptSort }) {
+    async function handleFormSubmit(evt) {
+        evt.preventDefault();
+        acceptSort( sort === '1' ? '0' : '1');
+    }
+
+    return (
+        <form className="horizontal" onSubmit={handleFormSubmit}>
+            {sort === '1' &&
+                <input type="submit" value="Вывести все вместе"/>}
+
+            {sort === '0' && 
+                <input type="submit" value="Вывести выполненные дела в конце"/>}
+        </form>
+    );
+}
