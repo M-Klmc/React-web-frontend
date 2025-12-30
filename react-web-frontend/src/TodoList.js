b13df41d5b732018f8dbae4d2bdbbac2974e4c2d
Mikhail <041411032425mk@gmail.com>
2025-12-30 16:00:19 +0400

    upd TodoList.js

--- react-web-frontend/src/TodoList.js
+++ react-web-frontend/src/TodoList.js
@@ -1,13 +1,30 @@
 import { useEffect, useState } from "react";
 import { baseURL } from "./utility.js";
 
-import TodoItem from './TodoItem.js' 
+import TodoItem from './TodoItem.js';
+import TodoSearch from './TodoSearch.js';
+import TodoSort from './TodoSort';
 
 export default function TodoList({ token }) {
     const [todos, setTodos] = useState([]);
+    const [search, setSearch] = useState('');
+    const [sort, setSort] = useState('0');
+
+    function acceptSearch(searchWord) {
+        setSearch(searchWord);
+    }
+
+    function acceptSort(sortOrder) {
+        setSort(sortOrder)
+    }
+
     useEffect(() => {
         (async() => {
-            const response = await fetch(baseURL, {
+            const params = new URLSearchParams();
+            if (search)
+                params.append('search', search);
+                params.append('doneAtLast', sort);
+            const response = await fetch(`${baseURL}?${params}`, {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
@@ -17,9 +34,11 @@ export default function TodoList({ token }) {
             else
                 window.alert(response.status + ':' + response.statusText);
         })();
-    }, [token]);
+    }, [token, search, sort]);
     return (
         <>
+            <TodoSearch search={search} acceptSearch={acceptSearch} />
+            <TodoSort sort={sort} acceptSort={acceptSort}/>
             <h1 className="heading">Запланированные дела</h1>
             {todos.map((item) => (
                 <TodoItem key={item._id} item={item}/>
