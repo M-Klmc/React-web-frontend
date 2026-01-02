f24cf9ebe165a12faf75a36a22b63783f50a72e8
Mikhail <041411032425mk@gmail.com>
2026-01-02 19:00:03 +0400

    added login.js and upd app.js

--- react-web-frontend/src/App.js
+++ react-web-frontend/src/App.js
@@ -1,18 +1,32 @@
-import TodoList from './TodoList.js'
+import { useState } from 'react';
+
+import TodoList from './TodoList.js';
+import Login from '/Login.js';
+import Logout from '/Logout.js'
 
 function App() {
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiTS1LbG1jMiIsImlhdCI6MTc2NzAzMTI2OX0.QaeAUhOOxGFX5C3pOGIiCDaaRdUafBUSGWLNf7Cm_-k';
+  const [token, setToken] = useState(undefined);
+
+  function acceptToken(tnk) {
+    setToken(tnk);
+  }
+
   return (
     <> 
       <nav>
+        {token &&
         <a href='/' className='brand'>
           <span>Список дел</span>
         </a>
+        }
         <input id='bmenub' type='checkbox' className='show' />
         <label htmlFor='bmenub' className='burger pseudo button'>&#9776</label>
-        <div className='menu'></div>
+        <div className='menu'>
+              {token && <Logout acceptToken={acceptToken}/>}
+        </div>
       </nav>
-      <TodoList token={token}/>
+        {!token && <Login acceptToken={acceptToken}/>}
+        {token && <TodoList token={token}/>}
       <p className='copyright'>Все права принадлежат M-Klmc</p>
     </>
   );
