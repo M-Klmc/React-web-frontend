7c49923c4c803310e82fd482b36f0668fb6bd96a
Mikhail <041411032425mk@gmail.com>
2026-01-07 23:56:09 +0400

    upd TodoItem

--- react-web-frontend/src/TodoItem.js
+++ react-web-frontend/src/TodoItem.js
@@ -1,15 +1,16 @@
 import TodoSetDone from './TodoSetDone.js';
 import TodoDelete from './TodoDelete.js';
+import { Link } from 'react-router-dom';
 
 export default function TodoItem({ item }) {
     return (
         <article className="card">
             <header>
                 {item.done &&
-                    <h2><del></del><a href="/">{item.title}</a></h2>
+                    <h2><del><Link to={`/${item.id}`}>{item.title}</Link></del></h2>
                 }
                 {!item.done &&
-                    <h2><a href="/">{item.title}</a></h2>
+                    <h2><del><Link to={`/${item.id}`}>{item.title}</Link></del></h2>
                 }
             </header>
             <footer>
