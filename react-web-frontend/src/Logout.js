c214e89be044a60c464637e885566f988fd076f0
Mikhail <041411032425mk@gmail.com>
2026-01-07 21:44:06 +0400

    add navigate to Logout

--- react-web-frontend/src/Logout.js
+++ react-web-frontend/src/Logout.js
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Logout({ acceptToken }) {
+    const redirect = useNavigate();
+
     function handleFormSubmit(evt) {
         evt.preventDefault();
         acceptToken(undefined);
+        redirect('/login');
     }
 
     return (
